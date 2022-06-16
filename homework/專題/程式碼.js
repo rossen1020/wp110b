505833af292ad244817a4f1bce6638145141529b
rossen1020 <99935090+rossen1020@users.noreply.github.com>
2022-06-17 02:31:57 +0800

    Update 程式碼.js

--- homework/專題/程式碼.js
+++ homework/專題/程式碼.js
@@ -1,180 +1,153 @@
 var canvas = document.getElementById('game');
-var Context = canvas.getContext('2d');
+var ctx = canvas.getContext('2d');
 
 
-var snakePart = [];
-class SnakePart{
-    constructor(x, y){
-        this.x = x;
-        this.y = y;
-    }
-}
-
-//遊戲初始設定
 var speed = 8;
-var tileSize = (canvas.width / 20) - 2;//設定活動範圍
-var snakeX = 10, snakeY = 10;
-var long = 0;
-var appleX = 5, appleY = 5;
-var Vx = 0, Vy = 0;//初始速度
+var lattice = 20;//要設20*20的格數
+var Size = canvas.width / lattice ;//方格大小
+var X = 10, Y = 10;//初始位置:中心點
+var appleX = 10, appleY = 5;//蘋果位置
+var Vx = 0, Vy = 0;//位置控制
 var score = 0;
 
-
+//遊戲主架構
 function startGame() {
-    snakePosition();
-    var lose = isOver();
-    if(lose){
-        document.body.addEventListener('keydown', playAgain);
-        clearScreen();
-    		checkColli();
-        return;
-    }
-		else if(win){
-        return;
-    }
-    
-   // var win = isWin();
+  
 
-    drawApple();
-    drawSnake();
-    drawScore();
-    
-    setSpeed();
-    
-    setTimeout(startGame, 1000/speed);
+  var lose = isOver();//結束與否
+  if (lose) {
+    document.body.addEventListener('keydown', playAgain);
+    //按空白鍵重新開始
+    return;
+  }
+
+  var win = isWin();
+  if (win) {
+    return;
+  }
+
+  snakePosition();//調整snake的位置
+  backgroun();//背景樣式
+  checkColli();//吃到蘋果
+  Apple();//蘋果樣式
+  Snake();//蛇的樣式
+  Score();//分數
+  Speed();//調整速度
+
+  setTimeout(startGame, 1000 / speed);
 }
 
-//蛇的移動
-function snakePosition() {
-    snakeX = snakeX +Vx;
-    snakeY = snakeY +Vy;
+//調整速度
+function Speed() {
+  if (score == 3) {
+    speed = 13;
+  }
+  if (score >= 4) {
+    speed +2;
+  }
 }
 
-function setSpeed() {
-        if(score == 5){
-            speed = 10;
-        }    
-}
-
+//贏的條件
 function isWin() {
     var win = false;
-    if(score == 10){
-        win = true;
+  if (score == 10) {
+    win = true;
+        
+    ctx.font="30px Verdana";
+    var gradient=ctx.createLinearGradient(0,0,canvas.width,0);
+    gradient.addColorStop("0","DeepPink");
+    gradient.addColorStop("0.5","Cyan");
+    gradient.addColorStop("1.0","BlanchedAlmond");
+    ctx.strokeStyle=gradient;
+    ctx.strokeText("you are winner!",canvas.width/5, canvas.height /2);
+        
     }
-    if(win){
-        Context.fillStyle = "white";
-        Context.font = "50px Poppins";
-        Context.fillText("你贏了!", canvas.width/3.3, canvas.height /2)
-    }
-    return win;
+  return win;
 }
 
+//輸的條件
 function isOver() {
-    var Over = false;
-    if(snakeX < 0 || snakeX == 20 || snakeY < 0 || snakeY == 20){
-        Over = true;
-    }
-    for(var i = 0; i < snakePart.length; i++){
-        if(snakeX == snakePart[i].x && snakeY == snakePart[i].y){
-            Over = true;
-        }
-    }
-    if(Over){
-        Context.fillStyle = "white";
-        Context.font = "50px Poppins";
-        Context.fillText("Game Over!", canvas.width/6.5, canvas.height /2);
-        Context.font = "40px Poppins";
-        Context.fillText("再玩一次?", canvas.width/3.5, canvas.height /2 + 50 );
-        Context.font = "25px Poppins";
-        Context.fillText("按空白鍵", canvas.width/2.7, canvas.height /2 +100 );
-    }
-    return Over;
-}
-/*
-function clearScreen() {
-    Context.fillStyle= 'black';
-    Context.fillRect(0, 0, 400, 400);
-}
-
-function drawSnake() {
+  let Over = false;
+  if (X < 0 || X == 20 || Y < 0 || Y == 20) {
+    Over = true;
     
-    Context.fillStyle = "green";
-    for(var i = 0; i< snakePart.length; i++){
-        var part = snakePart[i];
-        Context.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
-    }
+    ctx.fillStyle = "Crimson";
+    ctx.font = "50px Poppins";
+    ctx.fillText("Game Over!", canvas.width / 6.5, canvas.height / 2);
+    ctx.font = "25px Poppins";
+    ctx.fillText("按空白鍵", canvas.width / 2.7, canvas.height / 3 + 100);
+  }
 
-    snakePart.push( new SnakePart(snakeX, snakeY));
-    if(snakePart.length > tailLen){
-        snakePart.shift();
-    }
+  return Over;
+}
 
-    Context.fillStyle = 'orange';
-    Contextx.fillRect(snakeX * tileCount, snakeY *tileCount, tileSize, tileSize);
+//遊戲背景
+function backgroun() {
+  ctx.fillStyle = '#00163a';
+  ctx.fillRect(0, 0, 400, 400);
+}
+
+function Snake() {
+
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(X * lattice, Y * lattice, Size, Size);
 
 }
 
-function drawApple() {
-    Context.fillStyle = "red";
-    Context.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize);
+//蘋果樣式
+function Apple() {
+  ctx.fillStyle = "green";
+  ctx.fillRect(appleX * lattice, appleY * lattice, Size, Size);
+  //為繪製"已填充"的矩形 fillRect(x, y, 寬, 高)
 }
 
-function drawScore() {
-    Context.fillStyle = "white";
-    Context.font = "10px Poppins";
-    Context.fillText("Score: " + score, canvas.width-50, 10);
+function Score() {
+  ctx.fillStyle = "white";//字體顏色
+  ctx.font = "15px Poppins";//大小
+  ctx.fillText("Score: " + score, canvas.width - 60, 20);
+  //設定score文字及位置
 }
 
 function checkColli() {
-    if(appleX === headX && appleY === headY){
-        appleX = Math.floor(Math.random() * tileCount);
-        appleY = Math.floor(Math.random() * tileCount);
-        tailLen ++;
-        score ++;
-        if(score > 5 && score % 2 == 0){
-            speed ++;
-        }
-    }
-}*/
+  if (appleX == X && appleY == Y) {
+    appleX = Math.floor(Math.random() * lattice);
+    appleY = Math.floor(Math.random() * lattice);
+    //x,y 隨機生成蘋果
+    score++;
+  }
+}
 
-
-
+//調整snake的位置
+function snakePosition() {
+  X = X + Vx;
+  Y = Y + Vy;
+}
+//控制鍵
 document.body.addEventListener('keydown', keyDown);
-
 function keyDown(event) {
 
-    //go up
+    //向上
     if(event.keyCode== 38){
-        if(yV == 1) 
-            return;
-        yV = -1;
-        xV = 0;
+        Vx = 0;
+        Vy = -1;
     }
-
-    //go down
+    //向下
     if(event.keyCode == 40){
-        if(yV == -1) 
-            return;
-        yV = 1;
-        xV = 0;
+        Vx = 0;
+        Vy = 1;
     }
-
-    //go left
+    //向左
     if(event.keyCode == 37){
-        if(xV == 1) 
-            return;
-        yV = 0;
-        xV = -1;
+        Vx = -1;
+        Vy = 0;
     }
-
-    //go right
+    //向右
     if(event.keyCode == 39){
-        if(xV == -1) 
-            return;
-        yV = 0;
-        xV = 1;
+        Vx = 1;
+        Vy = 0;
     }
 }
+//空白鍵重新
 function playAgain(event) {
     if(event.keyCode == 32){
         location.reload();
