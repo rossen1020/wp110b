1bad7655de9912283686ea5d0ec127c44a2fc752
rossen1020 <99935090+rossen1020@users.noreply.github.com>
2022-06-17 09:49:23 +0800

    Create 12.js

---
+++ homework/習題5/12.js
@@ -0,0 +1,27 @@
+class Ratio{
+  constructor (a,b){
+    this.a=a;
+    this.b=b;
+  }
+  
+  sub1(i){
+  return new Ratio(this.a*i.b , this.b*i.b);
+  }
+  sub2(j){
+  return new Ratio(this.b*j.a , 0);
+  }
+  
+  add(p){
+    return new Ratio(this.a+p.a , this.b);
+  }
+  toString()
+    {
+        return this.a + "/" + this.b;
+    }
+}
+
+var r1 = new Ratio(1,3), r2 = new Ratio(2,4); 
+var  n= r1.sub1(r2);
+var  m= r1.sub2(r2);
+var r3 = n.add(m); 
+console.log(r3.toString());
