4a69e7cd490ba937d96016eca51f5fea118862a6
rossen1020 <99935090+rossen1020@users.noreply.github.com>
2022-06-17 10:15:32 +0800

    Update 13.js

--- homework/習題5/13.js
+++ homework/習題5/13.js
@@ -14,6 +14,13 @@ class Ratio{
   add(p){
     return new Ratio(this.a+p.a , this.b);
   }
+  mul(q){
+    return new Ratio(this.a-q.a , this.b);
+  }
+  div(k) {
+    return new Ratio(this.a/k.a , this.b);
+  }
+  
   toString()
     {
         return this.a + "/" + this.b;
@@ -23,5 +30,7 @@ class Ratio{
 var r1 = new Ratio(1,3), r2 = new Ratio(2,4); 
 var  n= r1.sub1(r2);
 var  m= r1.sub2(r2);
-var  = n.add(m); 
+var  h= n.add(m);
+var  s= h.mul(m);
+var  r3= s.div(r2);
 console.log(r3.toString());
